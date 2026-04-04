d9f3abba847b14b81a222fa6e973f1f8075af375
vuhoang2708 <vuhoang2708@gmail.com>
2026-04-04 13:27:30 +0700

    feat: Update CRM Webhook and Sheet to new dedicated endpoint

--- tracking.js
+++ tracking.js
@@ -1,5 +1,5 @@
 // tracking.js - Hệ thống Analytics hợp nhất cho Dự án DH4HN
-const SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzTHPGcQoUPluiwHpBU0gROVEf_Yv5ndg_5zZ-YYKQ6OoaF3wIDx8GEqYBanCfVYMHA/exec";
+const SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxxbba8bvb7H2Em179HgJUv0Tj8dnxWIuGynmVqjDcPVwADrTBXxx7UwE5AKroIQR5i/exec";
 const sessionId = 'dh-' + Date.now() + '-' + Math.random().toString(36).substr(2, 5);
 
 window.sessionId = sessionId; // Export cho quiz.js dùng chung
